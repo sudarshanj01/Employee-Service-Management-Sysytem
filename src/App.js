ef5e9c51fa6fefdf04b75e7d66399c5b901108a0
sudarshanj01 <sudarshan08062001@gmail.com>
2023-04-18 17:56:14 +0530

    Intitial Comm.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter , Routes,Route } from 'react-router-dom';
+import EmployeeList from './components/EmployeeList';
+import AddEmployee from './components/AddEmployee';
+import UpdateEmployee from './components/UpdateEmployee';
+import NoPage from './components/NoPage';
+import  Navbar  from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        
+          <Route index element={<EmployeeList />}/>
+          <Route path="add" element={<AddEmployee />} />
+          <Route path="update/:id" element={<UpdateEmployee />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path='employeeList' element={<EmployeeList />}/>
+        
+      </Routes>
+    </BrowserRouter>
+
+   
   );
 }
 
